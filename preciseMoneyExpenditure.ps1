828aded9add92b12f552b1b819370ab97ad1c463
Sondre Jørgensen <sondrjor@stud.ntnu.no>
2021-11-16 09:10:07 +0100

    liten endring

--- preciseMoneyExpenditure.ps1
+++ preciseMoneyExpenditure.ps1
@@ -4,7 +4,7 @@
 
 $stringarray = ".*mcdonalds*.", ".*tacobell*.", ".*pizzahut*.", ".*starbucks*.", ".*walmart*."
 #$stringarray = ".*extra*.", ".*rema*.", ".*obs*.", ".*matkroken*.", ".*meny*."
-import-csv C:\Users\sonde\Desktop\OversiktKonti-01.09.2021-16.11.2021.csv | select-string $stringarray | export-csv C:\Users\$env:UserName\Desktop\spending.csv
+import-csv "filepath.csv" | select-string $stringarray | export-csv C:\Users\$env:UserName\Desktop\spending.csv
 $array = select-string -path C:\Users\$env:userName\Desktop\spending2.csv -pattern '"-[0-9]*' -allmatches
 [array]$array1 = select-string -path C:\Users\$env:userName\Desktop\spending.csv -pattern '"-[0-9]*' -allmatches | % { $_.Matches } | % { $_.Value }
 #https://stackoverflow.com/questions/12609760/i-would-like-to-color-highlight-sections-of-a-pipeline-string-according-to-a-reg
