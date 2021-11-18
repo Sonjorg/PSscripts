380026aac52c15a89dd3fe35f8ac7224c2251a6d
Sondre Jørgensen <sondrjor@stud.ntnu.no>
2021-11-18 01:09:57 +0100

    small change

--- deleteScheduledTask.ps1
+++ deleteScheduledTask.ps1
@@ -1,4 +1,6 @@
+#get list of all scheduled tasks
+Get-ScheduledTask
 #to disable it only:
 Get-ScheduledTask | where {$_.taskname -eq "name"} | disable-scheduledtask
 #to delete it:
-Get-ScheduledTask | where {$_.taskname -eq "newrestorepointdaily"} | Unregister-ScheduledTask
+Get-ScheduledTask | where {$_.taskname -eq "name"} | Unregister-ScheduledTask
