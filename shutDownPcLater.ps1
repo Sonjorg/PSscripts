7b4887c2ef229a1927e49de20301e94f9d684297
Sondre Jørgensen <sondrjor@stud.ntnu.no>
2021-11-18 01:08:27 +0100

    waketorun

--- shutDownPcLater.ps1
+++ shutDownPcLater.ps1
@@ -1,18 +1,22 @@
 #Shuts down your PC after x hours
 [float]$hours = read-host -prompt 'When to shut down PC(hours from now)?'
 $name = [System.IO.Path]::GetRandomFileName()
-$date = (get-date).addhours($hours)
-$tasktrigger = new-scheduledtasktrigger -Once -At $date
-$Settings = New-ScheduledTaskSettingsSet -WakeToRun
+$time2 = (get-date).addhours($hours)
+$tasktrigger = new-scheduledtasktrigger -Once -At $time2
 $taskaction = New-ScheduledTaskAction `
     -Execute 'powershell.exe' `
     -Argument '-NoProfile -WindowStyle Hidden -command "&{stop-computer}"'
 Register-Scheduledtask `
-    -settings $Settings `
     -TaskName $name `
     -Action $taskaction `
     -Trigger $tasktrigger
-$tid = (get-date)
-$log = "Task schedule created at $tid to shut down the PC at $date ! =)"
+
+$Settings = New-ScheduledTaskSettingsSet -WakeToRun
+set-scheduledtask `
+    -taskname $name `
+    -settings $Settings
+
+$time = (get-date)
+$log = "Task schedule created at $time to shut down the PC at $time2 ! =)"
 add-content -path "\users\$env:UserName\shutdownpclog.log" -value $log
 start-process -path "\users\$env:UserName\shutdownpclog.log"
