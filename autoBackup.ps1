167cda8db8e236ffab4fa776073ab001aa8c7321
Sonjorg <101078309+Sonjorg@users.noreply.github.com>
2022-06-20 01:50:16 +0200

    Update autoBackup.ps1

--- autoBackup.ps1
+++ autoBackup.ps1
@@ -1,11 +1,13 @@
+#"/E" makes sure subfolders gets copied and "/XO" exludes older files"
+#source robocopy: https://superuser.com/questions/671244/robocopy-to-copy-only-new-folders-and-files
+#If you want to run a scriptfile instead, replace "-Argument..." with 
+# -Argument '-File C:\users\yourfile.ps1'
+
 $tasktrigger = new-scheduledtasktrigger -AtStartup
 $taskaction = New-ScheduledTaskAction `
     -Execute 'powershell.exe' `
     -Argument '-NoProfile -WindowStyle Hidden `
     -command &{Robocopy "full path of source" "full path of destination" /E /XO}'
-    #"/E" makes sure subfolders gets copied and "/XO" exludes older files(doesn't override if source files are older than destination)"
-#source robocopy: https://superuser.com/questions/671244/robocopy-to-copy-only-new-folders-and-files
-
 Register-Scheduledtask `
     -TaskName 'backup' `
     -Action $taskaction `
