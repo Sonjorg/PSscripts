79afe6145fca5872726e9384d6ccce02983efc82
Sonjorg <101078309+Sonjorg@users.noreply.github.com>
2022-11-19 18:42:47 +0100

    add clean a listfile

--- oneliners.ps1
+++ oneliners.ps1
@@ -1,19 +1,27 @@
 #find file:
 Get-Childitem –Path C:\ -Include *filename* -Recurse
+
 #find folder:
 Get-ChildItem -path C:\ -recurse | Where-Object {$_.PSIsContainer -eq $true -and $_.Name -match "keyword"}
+
 #find newly accessed file, (edit last number for number of hours or use adddays()):
 Get-ChildItem -path C:\ -Include *filename* -recurse | Where-Object {$_.lastaccesstime -gt (get-date).addhours(-8)}
+
 #Sort items by size:
 get-childitem * | select name, length | sort { $_.length } -descending
+
+#Remove everything after first word for each entry in a list, for example after pasting from wikipedia
+$(foreach($line in Get-Content .\messylist.txt) {$line|foreach {$_ -replace '\s+.*', '' }}) | out-file cleanlist.txt
+
 #Clean disks
 cleanmgr /sagerun:1 | out-Null
+
 #scheduled tasks:
 #get list of all scheduled tasks
 Get-ScheduledTask
+
 #to disable it only:
 Get-ScheduledTask | where {$_.taskname -eq "name"} | disable-scheduledtask
+
 #to delete it:
 Get-ScheduledTask | where {$_.taskname -eq "name"} | Unregister-ScheduledTask
-
-#tip: use get-help before a cmdlet to see arguments or "| get-member" after to get objects and methods
